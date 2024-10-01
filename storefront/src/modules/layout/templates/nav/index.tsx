23e407033c51a637b504eb18d5fd4d4246326fb9
Darius <101217348+leonids21@users.noreply.github.com>
2024-10-02 00:25:26 +0300

    RMR-001 - Logo component (#1)

--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from "react"
 
-import { listRegions } from "@lib/data/regions"
 import { StoreRegion } from "@medusajs/types"
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
-import CartButton from "@modules/layout/components/cart-button"
+import { listRegions } from "@lib/data/regions"
+import Logo from "@modules/common/components/logo"
 import SideMenu from "@modules/layout/components/side-menu"
+import CartButton from "@modules/layout/components/cart-button"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
 
 export default async function Nav() {
   const regions = await listRegions().then((regions: StoreRegion[]) => regions)
@@ -20,15 +21,10 @@ export default async function Nav() {
           </div>
 
           <div className="flex items-center h-full">
-            <LocalizedClientLink
-              href="/"
-              className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
-              data-testid="nav-store-link"
-            >
-              Medusa Store
+            <LocalizedClientLink href="/" data-testid="nav-store-link">
+              <Logo width={42} height={'auto'}/>
             </LocalizedClientLink>
           </div>
-
           <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
             <div className="hidden small:flex items-center gap-x-6 h-full">
               {process.env.NEXT_PUBLIC_FEATURE_SEARCH_ENABLED && (
